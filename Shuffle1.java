74ef7c21cb97da6c6e6113bfe7772881ef35be65
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2018-02-21 12:25:56 +0530

    some logic

--- Shuffle1.java
+++ Shuffle1.java
@@ -4,17 +4,16 @@ class Shuffle1{
         while(x>0){
             if(x>2){
                 System.out.print("a");
-                System.out.print("-");              
+                             
             }
             x=x-1;
+            System.out.print("-"); 
             if(x==2){
                 System.out.print("b  c");
-                System.out.print("-");  
             }
-            x--;
             if(x==1){
-                System.out.println("d");
-                
+                System.out.print("d");
+                x--;
             }
         }
     }
